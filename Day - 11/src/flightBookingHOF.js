98ac729b54dace6ad795ba73af62c5bebd77cfe9
swapnilgupta14 <swapnilguptaswag@gmail.com>
2024-10-17 09:55:32 +0530

    Day - 11: UI-Flight booking App

---
+++ Day - 11/src/flightBookingHOF.js
@@ -0,0 +1,57 @@
+// 1
+const availableFlights = [
+    { flightNumber: 'AI101', from: 'New York', to: 'London', price: 700, availableSeats: 15 },
+    { flightNumber: 'AI102', from: 'London', to: 'Paris', price: 120, availableSeats: 18 },
+    { flightNumber: 'AI103', from: 'Kanpur', to: 'Lucknow', price: 400, availableSeats: 10 },
+    { flightNumber: 'AI104', from: 'Lucknow', to: 'Delhi', price: 500, availableSeats: 20 },
+    { flightNumber: 'AI105', from: 'Delhi', to: 'Mumbai', price: 560, availableSeats: 19 },
+];
+
+
+// 2
+const filterFlights = (availableFlights, predicateFuntion) => availableFlights.filter(predicateFuntion);
+// console.log(filterFlights(availableFlights, flight => flight.availableSeats > 15));
+// console.log(filterFlights(availableFlights, flight => flight.to === 'London'));
+
+
+// 3
+const sortFlights = (availableFlights, comparator) => availableFlights.sort(comparator);
+const comparator = (a, b) => a.price - b.price;
+sortFlights(availableFlights, comparator);
+// console.log(flightsSortedByPrice);
+
+
+
+// 4
+const calculateTotalRevenue = (availableFlights) => {
+    return availableFlights.reduce((acc, item) => acc + (item.price * item.availableSeats), 0);
+}
+// console.log(calculateTotalRevenue(availableFlights));
+
+
+// 5
+const predicate1 = flight => flight.to === 'Delhi';
+const predicate2 = flight => flight.price <= 500;
+const combinedFilters = [predicate1, predicate2];
+// const arr = [1, 2, false, 5, 6];
+// console.log(arr.every(item => item));
+const composeFilters = (predicateArray) => {
+    return (flight) => {
+        return predicateArray.every(predicate => predicate(flight));
+    };
+};
+const filteredFlights = filterFlights(availableFlights, composeFilters(combinedFilters));
+// console.log(filteredFlights);
+
+
+
+
+// 6
+const findFlightById = (availableFlights, flightNumber) => availableFlights.find(item => item.flightNumber === flightNumber);
+// console.log(findFlightById(availableFlights, 'AI103'));
+
+
+
+
+
+
