ce47f21a25692febb9357b991aba154a58f08253
swapnilgupta14 <swapnilguptaswag@gmail.com>
2024-11-04 16:29:36 +0530

    Day - 23

--- Day - 23 (React D-1)/src/App.js
+++ Day - 23 (React D-1)/src/App.js
@@ -3,8 +3,16 @@ import Pet from "./Pet.js";
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, { name: "Kittu", animal: "Dog", breed: "Havanese" }),
-    React.createElement(Pet, { name: "Pepper", animal: "Bird", breed: "Cockatiel" }),
+    React.createElement(Pet, {
+      name: "Kittu",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
     React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
   ]);
 };
